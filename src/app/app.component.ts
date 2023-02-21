fd46fc43af6c61e1066e335d42702a592e814596
romario <romarioidet10@gmail.com>
2023-02-21 10:36:36 -0500

    the first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'CalculatorApp';
+  title = 'Basic Calculator';
+
+  numero1:number = 0;
+  numero2:number = 0;
+  result: number = 0;
+
+  sumar():void{ // Method no return value
+    this.result = this.numero1 + this.numero2;
+  }
+
+  restar():void{
+    this.result = this.numero1 - this.numero2;
+  }
+
+  multiplicar():void{
+    this.result = this.numero1 * this.numero2;
+  }
+
+  dividir():void{
+    this.result = this.numero1 / this.numero2;
+    
+  }
 }
